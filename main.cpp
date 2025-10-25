354fc23ec7ebb078b567a3af2c30f9c9bf5f0e17
Egor <eip_spb@mail.ru>
2025-10-25 22:19:52 +0300

    pozdnyakov.egor

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+const unsigned MAX_U = 4294967295u; // Константа предела переполнения unsigned
+
+bool isPyth(unsigned a, unsigned b, unsigned c) {
+	if ((a != 0 && a > MAX_U / a) || // Проверка переполнения
+		(b != 0 && b > MAX_U / b) ||
+		(c != 0 && c > MAX_U / c)) {
+		throw 2; // Выброс кода ошибки переполнения из функции
+	}
+	bool p = a == b * b + c * c;
+	p = p || b * b == a * a + c * c;
+	p = p || c * c == a * a + b * b;
+	return p;
+}
+
+int main() {
+	using u_t = unsigned;
+	u_t a = 0, b = 0, c = 0;
+	std::cin >> a >> b;
+	std::size_t count = 0;
+
+	while (std::cin >> c) {
+		try {
+			count += isPyth(a, b, c) ? 1 : 0;
+		} catch (int code) { // Вылавливание ошибки переполнения из функции
+			std::cerr << "Overflow error\n";
+			return 2;
+		}
+		a = b;
+		b = c;
+	}
+	if (std::cin.eof()) {
+		std::cout << count;
+		std::cout << "\n";
+	}
+	else if (std::cin.fail()) { // Ошибка ввода
+		std::cerr << "Input error\n";
+		return 1;
+	}
+}
